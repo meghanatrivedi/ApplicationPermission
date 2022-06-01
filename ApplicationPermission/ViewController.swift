d20bc52dab3c9f45a3c711700bb4ca3e33965346
Meghana Trivedi <meghanatrivedi92@gmail.com>
2022-06-01 20:58:48 +0530

    Initial Commit

---
+++ ApplicationPermission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplicationPermission
+//
+//  Created by Meghna on 01/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
