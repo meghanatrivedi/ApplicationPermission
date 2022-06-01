3c5878c3c7eeca41e6b458b0ce6c29000303b31e
Meghana Trivedi <meghanatrivedi92@gmail.com>
2022-06-01 21:54:30 +0530

    application permission with new style with new pod

--- ApplicationPermission/ViewController.swift
+++ ApplicationPermission/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import SPPermission
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnPermission: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func btnPermissionClick(_ sender: UIButton) {
+        
+        SPPermission.Dialog.request(with: [.camera, .microphone, .notification], on: self)
+        
+        let isAllowedCamera = SPPermission.isAllowed(.camera)
+        let isDeniedMicrophone = SPPermission.isDenied(.microphone)
+        
+        SPPermission.request(.notification, with: {
+            // Callback
+        })
+    }
+    
 }
 
